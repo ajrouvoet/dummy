3c44d39106fddf240b887e36ae10f276aebe7d2b
A.J.Rouvoet <a.j.rouvoet@gmail.com>
2013-05-13 13:58:32 +0200

    Moved Script to model Collectors should be a module with generic collectors
    Untested, WIP

--- dummy/collectors/PassFail.py
+++ dummy/collectors/PassFail.py
@@ -1,19 +1,18 @@
 from dummy.collectors import Collector
-from dummy.models import Test
 
-class PassFail(Collector):
+class PassFail( Collector ):
 	""" A class for Pass/Fail collecting
 	"""
 
-	def __init__(self, type="value"):
-		assert type in Collector.TYPE_CHOICES, "Unknown collector type: `%s`" % type
-		self.type = type
+	def __init__( self ):
+		super( type="value" )
 
-	def collect(self, test):
-		#mimic script
-		output = 'FAIL'
-		with open('test.path' + 'sim_out.txt', 'r') as sim_out:
-			for line in sim_out:
+	def collect( self, test ):
+		# mimic script
+		result = 'FAIL'
+		with open( test.log_path(), 'r' ) as log:
+			for line in log:
 				if 'PASS' in line:
-					output = 'PASS'
-		return self.parse_output(output)
+					result = 'PASS'
+
+		return self.parse_output( result )
