d4fd82437700479f089a18aa54a2a2e2eb0ca483
calavoow <calavoow@server.fake>
2013-05-27 16:26:03 +0200

    Aborted attempt at adding a quickstart functionality.

--- dummy/runner/__init__.py
+++ dummy/runner/__init__.py
@@ -198,3 +198,14 @@ def show( args ):
 		f( *args.metric )
 	else:
 		f( plot=args.plot )
+
+# def quickstart( args ):
+# 	logger.info( "Welcome to Dummy quickstart, where we configure your dummy_config.py" )
+# 	logger.info( "Are you sure you wish to continue?" )
+# 	choice = raw_input( "[y/n]" )
+# 	if choice != "y":
+# 		logger.warning( "Aborting..." )
+# 
+# 	logger.info( "Copying default configuration to current directory." )
+# 	(file, default_path, description) = imp.find_module( dummy.config.defaults )
+# 	shutil.copy( default_path, 'dummy_config.py' )
