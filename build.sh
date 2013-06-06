fe2bca939cafebf82e6be64e07966d74b062129f
A.J.Rouvoet <a.j.rouvoet@gmail.com>
2013-06-06 17:19:42 +0200

    Fixed build now dummy is in it's own package again

--- build.sh
+++ build.sh
@@ -8,10 +8,5 @@ then
 	VERSION=`git describe --always`
 fi
 
-# creating the dist dir
-rm -rf dist/
-mkdir dist/
-cp -rf src dist/dummy
-
 # tar that shit
-tar -cvzf "dummy-$BRANCH-$VERSION.tgz" -C dist dummy
+tar -cvzf "dummy-$BRANCH-$VERSION.tgz" -C src dummy
