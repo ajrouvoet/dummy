6250ff2f6bb62a3cda7a64fa0ebf2239f3fefd2a
A.J.Rouvoet <a.j.rouvoet@gmail.com>
2013-06-15 15:53:49 +0200

    Fixed setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
-
 from setuptools import setup
 
 setup(
-		name='Dummy',
-		version='0.5',
-		description='A generic testing framework',
-		author='A.J. Rouvoet, E. Schoute and A.B. Booij',
-		author_email='a.j.rouvoet_at_gmail.com',
-		include_package_data=True,
-		install_requires=[
-    		'colorlog',
-			'termcolor',
-			'python-dateutil',
-			'numpy',
-			'matplotlib'
-    		],
-		packages=[
-    		'dummy',
-    		],
-		scripts = [
-    		'scripts/dummy.sh',
-    		],
-		)
+	name='dummy',
+	version='0.6.1',
+	description='A test results framework',
+	author='A.J. Rouvoet, E. Schoute and A.B. Booij',
+	author_email='a.j.rouvoet_at_gmail.com',
+	include_package_data=True,
+	install_requires=[
+    	'colorlog',
+		'termcolor',
+		'python-dateutil',
+		'numpy',
+		'matplotlib'
+    ],
+	packages=[
+    	'dummy',
+    ],
+	scripts = [
+    	'bin/dummy',
+    ],
+)
