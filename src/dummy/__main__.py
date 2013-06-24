c6dba2576b2cb8d1fcc9fffc2a66812b70dce7aa
calavoow <calavoow@server.fake>
2013-06-24 16:34:45 +0200

    Remade argparsing into classes

--- src/dummy/__main__.py
+++ src/dummy/__main__.py
@@ -39,116 +39,145 @@ sys.path.append(
 	os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../' ))
 )
 
-parser = argparse.ArgumentParser( description='Dummy test aggregation' )
-sub = parser.add_subparsers()
+class CommonArguments( argparse.ArgumentParser ):
+	"""Arguments common to all dummy options.
+	"""
+	def __init__( self, **kwargs ):
+		kwargs['add_help'] = False
+		super( CommonArguments, self ).__init__( **kwargs )
+		
+		self.add_argument(
+			'tests',
+			help="names of the tests to run",
+			nargs="*"
+		)
+		self.add_argument(
+			'-S',
+			'--suite',
+			help="names of the suites to run",
+			action="append",
+			default=[]
+		)
+		self.add_argument(
+			'-x',
+			'--exclude',
+			help="Exclude a test to be run",
+			action="append",
+			default=[]
+		)
+		self.add_argument(
+			'-t',
+			'--target',
+			help="Run a specific target",
+			action="append",
+			default=[]
+		)
+		self.add_argument(
+			'-T',
+			'--alltargets',
+			help="Run all targets",
+			action="store_true",
+		)
+		self.add_argument(
+			'-c',
+			'--commit',
+			help="Run tests against a specific commit",
+			action="store"
+		)
 
-# debug switch
-parser.add_argument(
-	'-D',
-	'--debug',
-	help="output stacktrace on error",
-	action="store_true"
-)
+class DummyArgparser( argparse.ArgumentParser ):
+	@classmethod
+	def build( cls, **kwargs ):
+		""" Build a Dummy Argparser.
 
-common_args = argparse.ArgumentParser( add_help=False )
-common_args.add_argument(
-	'tests',
-	help="names of the tests to run",
-	nargs="*"
-)
-common_args.add_argument(
-	'-S',
-	'--suite',
-	help="names of the suites to run",
-	action="append",
-	default=[]
-)
-common_args.add_argument(
-	'-x',
-	'--exclude',
-	help="Exclude a test to be run",
-	action="append",
-	default=[]
-)
-common_args.add_argument(
-	'-t',
-	'--target',
-	help="Run a specific target",
-	action="append",
-	default=[]
-)
-common_args.add_argument(
-	'-T',
-	'--alltargets',
-	help="Run all targets",
-	action="store_true",
-)
-common_args.add_argument(
-	'-c',
-	'--commit',
-	help="Run tests against a specific commit",
-	action="store"
-)
+			Builder pattern is used to prevent endless recursion.
 
-# `dummy run [-s] <name>`
-runner = sub.add_parser( 'run', help="run tests", parents=[ common_args ])
-runner.set_defaults( func='run' )
-runner.add_argument(
-	'-!',
-	'--complement',
-	help="Only run tests for which no results exist",
-	action="store_true"
-)
-runner.add_argument(
-	'-n',
-	'--dryrun',
-	help="Prevent storage of the test results",
-	action="store_true"
-)
+			:returns argparse.ArgumentParser: configured for dummy.
+		"""
+		kwargs[ 'description' ] = "Dummy test aggregation"
+		parser = cls( **kwargs )
+		# debug switch
+		parser.add_argument(
+			'-D',
+			'--debug',
+			help="output stacktrace on error",
+			action="store_true"
+		)
+		
+		# Add subcommands
+		commands = parser.add_subparsers( help = "Dummy commands" )
+		parser.add_run( commands )
+		parser.add_stat( commands )
+		parser.add_show( commands )
 
-stat = sub.add_parser( 'stat', help="Statistics gathering", parents=[ common_args ])
-stat.set_defaults( func='stat', alltargets=False, complement=False )
-stat.add_argument(
-	'-m',
-	'--metric',
-	help="Show a specific metric or multiple metrics",
-	action="append",
-	default=[]
-)
-stat.add_argument(
-	'-s',
-	'--stat',
-	help="Calculate the named stat",
-	action="append",
-	default=[]
-)
+		return parser
+		
+	def add_run( self, subparser ):
+		""" Add the run command to subparser.
+		"""
+		runner = subparser.add_parser( 'run', help="run tests", parents=[ CommonArguments() ])
+		runner.set_defaults( func='run' )
+		runner.add_argument(
+			'-!',
+			'--complement',
+			help="Only run tests for which no results exist",
+			action="store_true"
+		)
+		runner.add_argument(
+			'-n',
+			'--dryrun',
+			help="Prevent storage of the test results",
+			action="store_true"
+		)
 
-show = sub.add_parser( 'show', help="results browsing", parents=[ common_args ] )
-show.set_defaults( func='show', complement=False )
-show.add_argument(
-	'-m',
-	'--metric',
-	help="Show a specific metric or multiple metrics",
-	action="append",
-	default=[]
-)
-show.add_argument(
-	'-p',
-	'--plot',
-	help="Show results in a plot (shortcut for --format=plot)",
-	action="store_true"
-)
-show.add_argument(
-	'--format',
-	help="Specify the formatter for the results",
-	action="store"
-)
+	def add_stat( self, subparser ):
+		""" Add the stat command to subparser.
+		"""
+		stat = subparser.add_parser( 'stat', help="Statistics gathering", parents=[ CommonArguments() ])
+		stat.set_defaults( func='stat', alltargets=False, complement=False )
+		stat.add_argument(
+			'-m',
+			'--metric',
+			help="Show a specific metric or multiple metrics",
+			action="append",
+			default=[]
+		)
+		stat.add_argument(
+			'-s',
+			'--stat',
+			help="Calculate the named stats",
+			action="append",
+			default=[]
+		)
 
-# `dummy quickstart`
-# quickstart = sub.add_parser( "quickstart", help="Quickly set up dummy config")
-# quickstart.set_defaults( func="quickstart" )
+
+	def add_show( self, subparser ):
+		""" Add the show command to subparser.
+		"""
+		show = subparser.add_parser( 'show', help="results browsing", parents=[ CommonArguments() ] )
+		show.set_defaults( func='show', complement=False )
+		show.add_argument(
+			'-m',
+			'--metric',
+			help="Show a specific metric or multiple metrics",
+			action="append",
+			default=[]
+		)
+		show.add_argument(
+			'-p',
+			'--plot',
+			help="Show results in a plot (shortcut for --format=plot)",
+			action="store_true"
+		)
+		show.add_argument(
+			'--format',
+			help="Specify the formatter for the results [default is log]",
+			action="store",
+			default="log"
+		)
 
 def main():
+	parser = DummyArgparser.build()
 	args = parser.parse_args()
 
 	# set the logging level
